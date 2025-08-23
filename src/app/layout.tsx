cfb34f0c8cdf0625a047b8a7daf1731f793873f2
Anirbandasjoy <joy600508@gmail.com>
2025-08-23 20:11:30 +0600

    add pixel code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
 import type { Metadata } from "next";
 import {
   Geist,
@@ -10,12 +12,9 @@ import {
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
 import ReduxWrapper from "@/redux/ReduxWrapper";
-// import TawkToChat from "@/components/TawkToChat";
-// import CallButton from "@/components/layout/Home/shared/CallButton";
 import { GoogleTagManager } from "@next/third-parties/google";
 import { GTMPageViewTracker } from "@/components/GTMPageViewTracker";
 import RightClickDisable from "@/components/RightClickDisable";
-// import FacebookMessengerButton from "@/components/layout/Home/shared/MessengerChat";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -60,6 +59,35 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* ✅ Facebook Pixel Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1783814055829519');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1783814055829519&ev=PageView&noscript=1"
+          />
+        </noscript>
+        {/* ✅ End Facebook Pixel */}
+      </head>
+
       <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM_ID!} />
 
       <ReduxWrapper>
